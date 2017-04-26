ca0ff42b3fd0ee3c40680de4b0ef820dee88730d
Jason Mandel <jmandel1027@gmail.com>
2017-04-25 22:48:28 -0400

    added more control to sound

--- public/main.js
+++ public/main.js
@@ -25,24 +25,27 @@ function setup() {
 }
 
 function draw() {
-  background(map(rotationX, 0.00, 0.99, 0, 255), map(rotationY, 0.00, 0.99, 0, 255), map(rotationZ, 0.00, 0.99, 0, 255));
+  //background(map(rotationX, 0.00, 0.99, 0, 255), map(rotationY, 0.00, 0.99, 0, 255), map(rotationZ, 0.00, 0.99, 0, 255));
 
-  //background(map(roll, -180, 180, 0, 255), map(pitch, -180, 180, 0, 255), map(yaw, -180, 180, 0, 255));
+  background(map(round(roll), -180, 180, 0, 255), map(round(pitch), -180, 180, 0, 255), map(round(yaw), -180, 180, 0, 255));
   //background( deviceTurned(roll), deviceTurned(pitch), deviceTurned(yaw) );
 
-  /*
-  var freq = map(rotationX, 0.00, 0.99, 0, 255);
+  var freq = map(round(roll), -180, 180, 0, 255);
+  //var freq = map(rotation, 0.00, 0.99, 0, 255);
   osc.freq(freq);
 
-  var amp = map(rotationY, 0.00, 0.99, 0, 255);
+  var amp = map(round(yaw), -180, 180, 0, 255);
+  //var amp = map(rotationY, 0.00, 0.99, 0, 255);
   osc.amp(amp);
 
-
-  filterFreq = map(rotationX, 0.00, 0.99, 0, 255);
-  filterWidth = map(rotationZ, 0.00, 0.99, 0, 255);
-  filterRes = map(rotationZ, 0.00, 0.99, 0, 255);
+  filterFreq = map(round(roll), -180, 180, 0, 255);
+  //filterFreq = map(rotationX, 0.00, 0.99, 0, 255);
+  filterWidth = map(round(yaw), -180, 180, 0, 255);
+  //filterWidth = map(rotationZ, 0.00, 0.99, 0, 255);
+  filterRes = map(round(pitch), -180, 180, 0, 255);
+  //filterRes = map(rotationZ, 0.00, 0.99, 0, 255);
   filter.set(filterFreq, filterWidth);
-  */
+
 
   if(frameCount < 5) {
    w0 = quaternion(rotationX, rotationY, rotationZ);
